dceac14ca0a71b782a031c19f967aaba6cb93b12
mwarnsley <marcus.warnsley@gmail.com>
2016-01-04 12:31:12 -0500

    Adding the css and javascript in the files for the likes and dislikes

--- click.js
+++ click.js
@@ -1,1 +1,12 @@
-ECHO is on.
+var lClicks = 0;
+var dClicks = 0;
+
+$(".likes").on("click", function(){
+	lClicks += 1;
+    document.getElementById("l-counter").innerHTML = lClicks;
+})
+
+$(".dislikes").on("click", function(){
+	dClicks += 1;
+    document.getElementById("d-counter").innerHTML = dClicks;
+})
